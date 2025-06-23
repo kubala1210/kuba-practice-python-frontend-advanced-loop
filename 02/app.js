1f2929ee2f0193972a59985e78fe0f626bb870e1
kubala1210 <jakubgromala@wp.pl>
2025-06-23 18:07:18 +0100

    update

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,18 @@
 const x = 5;
 let result = 0;
+
+for (let i = 0; i <= x; i++) {
+    result += i;
+}
+
+console.log(result);
+
+result = 0;
+let i = 0;
+
+while (i <= x) {
+    result += i;
+    i++;
+}
+
+console.log(result);
